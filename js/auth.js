fb9fce50239635f0c3d4764d670579030d390df7
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-09-09 07:14:14 +0200

    Just trying out some things

--- js/auth.js
+++ js/auth.js
@@ -4,19 +4,17 @@ class Auth {
         const auth = localStorage.getItem("token");
         this.validateAuth(auth);
     }
-
+    
     validateAuth(auth){
-        if(auth !== token){
+        if(auth !== `${token}`){
             window.location.replace("./")
         }else {
             document.querySelector("body").style.display = "block";
         }
     }
-
+    
     signOut(){
         localStorage.removeItem("auth");
         window.location.replace("./")
-    }
-
+    }   
 }
-
