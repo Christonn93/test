508960c84a222fa175768a682df57ca1d75b6eaf
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-09-23 14:20:25 +0200

    dsadasd

--- src/js/user/index.mjs
+++ src/js/user/index.mjs
@@ -5,10 +5,10 @@ const signIn = formAction.signIn;
 
 /**
  *
- * This is just a advent listener for the form on the login page. After the event have happened it runs the function to login the user whit "Email" and "password"
+ * This is just a add event listener for the form on the login page. After the event have happened it runs the function to login the user whit "Email" and "password"
  *
  */
-export async function signUserIn() {
+export async function formEvent() {
  try {
   const form = document.querySelector("form#login");
   form.addEventListener("submit", (e) => {
